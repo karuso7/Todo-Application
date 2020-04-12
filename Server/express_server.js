1fab9d051e5d2ab025761a4bb68c8e55a8bfecb5
Karsten Krause <karsten.krause88@gmx.de>
2020-04-12 21:52:55 +0200

    - Refactoring Login und Registrierung - Express-Server lauffähig aufgesetzt

--- Server/express_server.js
+++ Server/express_server.js
@@ -1,2 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+// Server starten
+app.listen(8080, "localhost", function () {
+    console.log("Server startet auf http://localhost:8080\n");
+    console.log("Testnutzer-Login mit:\n", "\t E-Mail: admin\n", "\t Passwort: admin\n");
+});
+// statische Routen
+app.use('/', express.static(__dirname + '/../client'));
+app.use(express.json());
--- Server/express_server.ts
+++ Server/express_server.ts
@@ -1,3 +1,16 @@
 import * as express from 'express';
 import {NextFunction, Request, Response} from "express";
 import session    = require ("express-session");
+
+
+let app = express();
+
+// Server starten
+app.listen(8080, "localhost", () => {
+    console.log("Server startet auf http://localhost:8080\n");
+    console.log("Testnutzer-Login mit:\n", "\t E-Mail: admin\n", "\t Passwort: admin\n");
+});
+
+// statische Routen
+app.use('/', express.static(__dirname + '/../client'));
+app.use(express.json());
